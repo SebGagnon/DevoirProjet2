56f1cc7dd4951d45c78565c7989e27868f8b6cfa
SebGagnon <sebasgagnon95@gmail.com>
2025-02-07 18:14:40 -0500

    fix pour l``espacement variable de l``axe y

--- GraphQstB.py
+++ GraphQstB.py
@@ -6,13 +6,17 @@ tension=[]
 courant=[]
 n=np.linspace(1,1000,1000)
 
-with open('Donnees.csv', 'r') as file:
+with open('Données-1000-Leila.csv', 'r') as file:
     entete=next(file)
     lecteur=csv.reader(file)
     for row in lecteur :
         tension.append(float(row[0]))
         courant.append(float(row[1]))
 
+espacement=[]
+for i in tension :
+    if i not in espacement :
+        espacement.append(i)
 moyenne=np.average(tension)
 ecart_type=np.std(tension)
 
@@ -21,7 +25,7 @@ fig, ax = plt.subplots()
 ax.scatter(n, tension, s=2)
 
 ax.set(xlim=(1, 1000), xticks=[1,100,200,300,400,500,600,700,800,900,1000],
-       ylim=(0.9995*np.min(tension), 1.0005*np.max(tension)), yticks=np.linspace(np.min(tension),np.max(tension),8))
+       ylim=(0.9995*np.min(tension), 1.0005*np.max(tension)), yticks=np.linspace(np.min(tension),np.max(tension),len(espacement)))
 ax.tick_params(right=True, top=True, labelright=False, labeltop=False, labelrotation=0)
 
 ax.set_ylabel('Tension (V)', fontsize=15)
